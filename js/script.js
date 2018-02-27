7cded8752d1c4b76cacf84c8d527a52783691f48
designerhub <perfectanil@gmail.com>
2018-02-27 09:38:48 +0530

    match logic added

--- js/script.js
+++ js/script.js
@@ -1,25 +1,61 @@
 
 
-var cardarry=["Card1","Card1","Card2","Card2"];
 
-var start = "<ul>";
-var end = "</ul>";
+var inputCardArray=["Vaibhav","Vaibhav","Anil","Anil","Avneet","Avneet","Shiv","Shiv"];
+
+var cardarry = randomMyArray(inputCardArray);
+
+var firstCardValue="";
+var secondCardValue="";
+var check=true;
+
+
+
 var result = "";
 
 for(var i=0; i<cardarry.length; i=i+1){
 
-	var liresult = '<span class="cardlist">'+cardarry[i]+'</span>';
+	var liresult = '<div class="click panel circle" onClick="cardClick('+i+')"><div class="front"><img src="images/card.png" width="190" height="300" alt=""></div><div class="back"><h2>'+cardarry[i]+'</h2> </div></div>'
 
 	result = liresult+result;
 	
 	if(i==1){
 			result = result + "<br>";
-		}
+	}
 }
-
-
 
 
 var element = document.getElementById("cardcontainer");
 
 element.innerHTML=result;
+
+
+function cardClick(str){
+
+  
+   //alert(cardarry[str]);
+
+
+   if(check){
+
+   	  firstCardValue =cardarry[str];
+   	  check=false;
+   }else{
+
+     secondCardValue = cardarry[str];
+
+      if(firstCardValue==secondCardValue){
+
+            alert("match");
+      }
+      else{
+         
+           alert("Not match");
+
+      }
+
+      check=true;
+
+   }
+
+}
